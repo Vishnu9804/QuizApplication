dee16e0006cae47ec28c9fda2259a1e5ede45b9f
Vishnu9804 <burkhawalavishnu@gmail.com>
2024-02-10 16:13:59 +0530

    Finish Commit

---
+++ app/src/main/java/com/example/quizapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.quizapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void next(View view) {
+        Intent intent = new Intent(MainActivity.this,sandn.class);
+        startActivity(intent);
+        finish();
+    }
+}
